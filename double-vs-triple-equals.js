9d85de466e53b9294bda10d9325819aa46d1a175
unknown <kawsarahmed4148@gmail.com>
2022-06-26 18:29:58 +0600

    == and === cleared

--- double-vs-triple-equals.js
+++ double-vs-triple-equals.js
@@ -1,6 +1,8 @@
+// Triple Equals ( === )  returns true if both operands are of the same type and contain the same value
+// Double Equals ( == ) checks for value equality only.
 const first = 2;
-const second = 2;
-if (first == second) {
+const second = '2';
+if (first === second) {
     console.log('Condition is True');
     
 }
